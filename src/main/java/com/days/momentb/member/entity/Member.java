88977901eca13c0939c298a23a341c366e07fed8
songyikim0911 <ksi6470@gmail.com>
2021-11-28 18:26:22 +0900

    security설정추가

---
+++ src/main/java/com/days/momentb/member/entity/Member.java
@@ -0,0 +1,35 @@
+package com.days.momentb.member.entity;
+
+
+import lombok.*;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import java.util.Set;
+
+@Entity//처음에 엔티티를 만들면 오류가 남 -> id가 없어서 그렇다. entity는 무조건 id를줘야함.
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter//엔티티는 가능하면 불변, 게터
+@ToString
+public class Member {
+
+    @Id
+    private String mid;
+
+    private String mpw;
+
+    private String mname;
+
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    private Set<MemberRole> roleSet;
+
+    public void changePassword(String password){
+        this.mpw = password;
+    }
+}
+
---
+++ src/main/java/com/days/momentb/member/entity/MemberRole.java
@@ -0,0 +1,9 @@
+package com.days.momentb.member.entity;
+
+
+public enum MemberRole {
+
+    USER,STORE,ADMIN;
+
+
+}
